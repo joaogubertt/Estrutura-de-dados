bd1c0b56e263e4e603697b1f1927170056163886
João Pedro Gubertt <107253777+joaogubertt@users.noreply.github.com>
2023-03-01 11:44:09 -0300

    Add files via upload

--- 2802.cpp
+++ 2802.cpp
@@ -10,12 +10,13 @@ struct lista
     int ultimo;
 };
 
-//Fun��o pegar� o item ultimo da struct e deixar� o valor em -1
+//Função pega o a variável ultimo da struct Lista e deixa o valor em -1
 void inicialiarLista(lista &Lista)
 {
     Lista.ultimo = -1;
 }
 
+//Função que percorre a lista dando cout até o char em indice lista.ultimo
 void mostrarLista(lista &Lista, char *frase)
 {
         cout << frase << ": ";
@@ -27,6 +28,7 @@ void mostrarLista(lista &Lista, char *frase)
             cout << Lista.item[i] << " ";
 }
 
+//Insere um char na ultima posição não preenchida da lista
 bool inserirFinalLista(lista &Lista, char valor)
 {
     if(Lista.ultimo == TAM-1) return false;
@@ -34,6 +36,7 @@ bool inserirFinalLista(lista &Lista, char valor)
     Lista.item[++Lista.ultimo] = valor;
 }
 
+//Insere conforme ordem alfabética, ['A', 'B', 'C', 'D']
 bool inserirOrdemLista(lista &Lista, char valor)
 {
     int pos=0;
@@ -48,6 +51,7 @@ bool inserirOrdemLista(lista &Lista, char valor)
     return true;
 }
 
+//Busca um char passado por parâmetro e retorna um valor booleano para mostrar ao usuário se determinado valor está no vetor. O valor retornado será o indice do resultado da busca em caso de existente
 int buscarLista(lista Lista, char valor)
 {
     for(int i = 0; i<= Lista.ultimo; i++)
@@ -57,6 +61,7 @@ int buscarLista(lista Lista, char valor)
     return -1;
 }
 
+//retirarLista utiliza da função de busca para retornar o indice e diminuir o indice uma casa de todos os valores acima do resultado da função, por fim diminui o valor de lista.ultimo
 bool retirarLista(lista &Lista, char valor)
 {
     int pos = buscarLista(Lista, valor);
@@ -68,6 +73,8 @@ bool retirarLista(lista &Lista, char valor)
     Lista.ultimo--;
     return true;
 }
+
+//O problema da função acima é que por exemplo em uma array ['A', 'B', 'C', 'D'] para remover o C a lista ficaria assim: ['A', 'B', 'D', 'D'], fazendo uma "gambiarra" para mostrarLista rodar somente até ['A', 'B', 'D'] pois o valor de Lista.ulitmo foi diminuido na linha 73
 
 int main()
 {
@@ -89,7 +96,6 @@ int main()
     mostrarLista(Lista1, "Lista 1");
 
     cout << endl;
-    //ERRO EM inserirOrdemLista(param1, param2)
 
     inicialiarLista(Lista2);
 
@@ -97,8 +103,16 @@ int main()
     inserirOrdemLista(Lista2, 'A');
     inserirOrdemLista(Lista2, 'B');
 
-
     mostrarLista(Lista2, "Lista 2");
+
+    char letraBusca = 'P';
+
+    cout << endl;
+
+    if(buscarLista(Lista1, letraBusca) == -1) 
+        cout << "Nao existe " << letraBusca << " em Lista1";
+    else
+        cout << "Existe " << letraBusca << " em Lista1 no indice " << buscarLista(Lista1, letraBusca);
 
     return 0;
 }
